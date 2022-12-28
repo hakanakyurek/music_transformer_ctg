8496e48a038c9dec15d50cf700cc3c40f4947eca
Hakan Akyürek <hakan.akyuerek@tum.de>
2022-12-28 15:34:00 +0100

    both print and log

--- lib/utilities/logging_config.py
+++ lib/utilities/logging_config.py
@@ -2,8 +2,13 @@ import logging
 import sys
 
 def config_logging():
+    logging.root.handlers = []
+
     logging.basicConfig(
         format="%(asctime)s [%(levelname)s] %(message)s",
-        filename="logs/debug.log",
-        level=logging.INFO
+        level=logging.INFO,
+        handlers=[
+            logging.FileHandler("logs/debug.log"),
+            logging.StreamHandler(sys.stdout)
+    ]
     )
