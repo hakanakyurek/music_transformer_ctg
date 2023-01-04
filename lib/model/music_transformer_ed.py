34502958f16aca17ad38bbf775b5f0d67d0a89f4
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-01-04 12:40:44 +0100

    target mask now fills with 1e-9 instead of -inf

--- lib/model/music_transformer_ed.py
+++ lib/model/music_transformer_ed.py
@@ -139,7 +139,7 @@ class MusicTransformerEncoderDecoder(pl.LightningModule):
         # Generates a squeare matrix where the each row allows one word more to be seen
         mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
         mask = mask.float()
-        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 0, float(-1e9)) # Convert zeros to -inf
         mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
         
         # EX for size=5:
