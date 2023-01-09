d3148d231875ef9b34d614fdfad5c6ca6f6e2e9c
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-01-09 14:30:55 +0100

    models should now be crrectly loaded form lightning checkpoints

--- lib/utilities/create_model.py
+++ lib/utilities/create_model.py
@@ -41,12 +41,12 @@ def create_model_for_generation(args):
                     d_model=args.d_model, dim_feedforward=args.dim_feedforward,
                     max_sequence=args.max_sequence, rpr=args.rpr).to(get_device())
 
-        model.load_state_dict(torch.load(args.model_weights, map_location=get_device()))
+        model.load_state_dict(torch.load(args.model_weights, map_location=get_device())['state_dict'])
     elif args.arch == 1:
         model = MusicTransformerEncoder(n_layers=args.n_layers, num_heads=args.num_heads,
                     d_model=args.d_model, dim_feedforward=args.dim_feedforward,
                     max_sequence=args.max_sequence, rpr=args.rpr).to(get_device())
 
-        model.load_state_dict(torch.load(args.model_weights, map_location=get_device()))
+        model.load_state_dict(torch.load(args.model_weights, map_location=get_device())['state_dict'])
 
     return model
