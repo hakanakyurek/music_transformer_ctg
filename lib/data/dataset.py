0a90f0bffc63f0ac7289c0f70fa856b0f4d12df0
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-01-19 10:30:38 +0100

    midi processing functions are moved to another file

--- lib/data/dataset.py
+++ lib/data/dataset.py
@@ -1,20 +1,14 @@
 import os
 import pickle
-import random
 import torch
-import torch.nn as nn
 from torch.utils.data import Dataset
 
-import logging
-
 from lib.utilities.constants import *
-from lib.utilities.device import cpu_device
-
 from lib.midi_processor.processor import RANGE_NOTE_ON, RANGE_NOTE_OFF
+from lib.data.midi_processing import process_midi, process_midi_ed
 
 import numpy as np
 
-SEQUENCE_START = 0
 
 class MidiDataset(Dataset):
     """
@@ -113,105 +107,3 @@ class MidiDataset(Dataset):
 
         del midi
         return aug_midi
-
-# process_midi
-def process_midi(raw_mid, max_seq, random_seq):
-    """
-
-    Takes in pre-processed raw midi and returns the input and target. Can use a random sequence or
-    go from the start based on random_seq.
-
-    """
-
-    x   = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE)
-    tgt = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE)
-
-    raw_len     = len(raw_mid)
-    full_seq    = max_seq + 1 # Performing seq2seq
-
-    if(raw_len == 0):
-        return x, tgt
-
-    # Shift to the right by one
-    if(raw_len < full_seq):
-        x[:raw_len]         = raw_mid
-        tgt[:raw_len-1]     = raw_mid[1:]
-        tgt[raw_len-1]      = TOKEN_END
-    else:
-        # Randomly selecting a range
-        if(random_seq):
-            end_range = raw_len - full_seq
-            start = random.randint(SEQUENCE_START, end_range)
-
-        # Always taking from the start to as far as we can
-        else:
-            start = SEQUENCE_START
-
-        end = start + full_seq
-
-        data = raw_mid[start:end]
-
-        x = data[:max_seq]
-        tgt = data[1:full_seq]
-
-
-    # print("x:",x)
-    # print("tgt:",tgt)
-
-    return x, tgt
-
-# process_midi for ed arch
-def process_midi_ed(raw_mid, max_seq, random_seq):
-    """
-
-    Takes in pre-processed raw midi and returns the input and target. Can use a random sequence or
-    go from the start based on random_seq.
-
-    """
-
-    x   = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE)
-    tgt_input = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE)
-    tgt_output = torch.full((max_seq, ), TOKEN_PAD, dtype=TORCH_LABEL_TYPE)
-
-    raw_len     = len(raw_mid)
-    full_seq    = max_seq + 1 # Performing seq2seq for ed arch
-
-    if(raw_len == 0):
-        return x, tgt_input, tgt_output
-
-    # Shift to the right by one
-    if(raw_len < full_seq):
-        x[:raw_len] = raw_mid
-        
-        tgt_input[0] = TOKEN_START
-        tgt_input[1:raw_len] = raw_mid[1:raw_len]
-
-        tgt_output[:raw_len - 1] = raw_mid[:raw_len - 1]
-        tgt_output[-1] = TOKEN_END
-    else:
-        # Randomly selecting a range
-        if(random_seq):
-            end_range = raw_len - full_seq
-            start = random.randint(SEQUENCE_START, end_range)
-
-        # Always taking from the start to as far as we can
-        else:
-            start = SEQUENCE_START
-
-        end = start + full_seq
-
-        data = raw_mid[start:end]
-
-        x = data[:max_seq]
-        
-        tgt_input[0] = TOKEN_START
-        tgt_input[1:full_seq - 1] = data[1:full_seq - 1]
-
-        tgt_output[:full_seq - 2] = data[:full_seq - 2]
-        tgt_output[-1] = TOKEN_END
-
-
-    # print("x:",x)
-    # print("tgt:",tgt)
-
-    return x, tgt_input, tgt_output
