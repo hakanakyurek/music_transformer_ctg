55ad68011d3f20de9f63dae0caa4fbc7df3e41c9
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-02-20 11:39:49 +0100

    updated preprocessing file

--- preprocess_midi.py
+++ preprocess_midi.py
@@ -6,72 +6,98 @@ import random
 
 from tqdm import tqdm
 
-import lib.midi_processor.processor as midi_processor
+from lib.midi_processor.processor import encode_midi, decode_midi
+import music21
+import pretty_midi
+
+
 
 JSON_FILE = "maestro-v2.0.0.json"
 
-# prep_midi
-def prep_maestro_midi(maestro_root, output_dir):
+
+def key_custom_midi(pieces, output_dir):
     """
 
-    Pre-processes the maestro dataset, putting processed midi data (train, eval, test) into the
-    given output folder
+    Author: Corentin Nelias
+
+    Pre-processes custom midi files that are not part of the maestro dataset, putting processed midi data (train, eval, test) into the
+    given output folder. 
 
     """
-
     train_dir = os.path.join(output_dir, "train")
     os.makedirs(train_dir, exist_ok=True)
-    val_dir = os.path.join(output_dir, "val")
-    os.makedirs(val_dir, exist_ok=True)
-    test_dir = os.path.join(output_dir, "test")
-    os.makedirs(test_dir, exist_ok=True)
-
-    maestro_json_file = os.path.join(maestro_root, JSON_FILE)
-    if(not os.path.isfile(maestro_json_file)):
-        print("ERROR: Could not find file:", maestro_json_file)
-        return False
-
-    maestro_json = json.load(open(maestro_json_file, "r"))
-    print(f"Found {len(maestro_json)} pieces")
-    print("Preprocessing...")
+    
 
     total_count = 0
-    train_count = 0
-    val_count   = 0
-    test_count  = 0
+    
+    keys = {}
 
-    for piece in maestro_json:
-        mid         = os.path.join(maestro_root, piece["midi_filename"])
-        split_type  = piece["split"]
-        f_name      = mid.split("/")[-1] + ".pickle"
-
-        if(split_type == "train"):
-            o_file = os.path.join(train_dir, f_name)
-            train_count += 1
-        elif(split_type == "validation"):
-            o_file = os.path.join(val_dir, f_name)
-            val_count += 1
-        elif(split_type == "test"):
-            o_file = os.path.join(test_dir, f_name)
-            test_count += 1
-        else:
-            print("ERROR: Unrecognized split type:", split_type)
-            return False
-
-        prepped = midi_processor.encode_midi(mid)
-
-        dump(prepped, o_file)
+    for piece in tqdm(pieces):
+            
+        try:
+            my_score: music21.stream.Score = music21.converter.parse(piece)
+            key = my_score.analyze('Krumhansl')
+            keys[piece] = str(key)
+        except Exception as e:
+            print(e)
+            continue
 
         total_count += 1
-        if(total_count % 50 == 0):
-            print(total_count, "/", len(maestro_json))
 
-    print(f"Num Train: {train_count}")
-    print(f"Num Val: {val_count}")
-    print(f"Num Test: {test_count}")
+    dump(keys, output_dir + 'keys')
+
+    print(f"Num total: {total_count}")
     return True
 
-def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2):
+
+def time_split(mid, enc, max_seq, keys=False, full_seq=False):
+    
+    encodings = []
+    # Get the end time of the whole midi
+    max_end_time = mid.get_end_time()
+    # Decode back the encoding
+    # Read time for max_seq - 1 tokens if we are using keys
+    max_seq = max_seq if not keys else max_seq - 1
+    
+    decoded_mid = decode_midi(enc[0:max_seq])
+    # Get the duration for clip
+    duration = decoded_mid.get_end_time()
+    
+
+    if full_seq:
+
+        start_time = 0
+
+        while start_time <= max_end_time:
+
+            # If the midi is shorter than max sequence
+            if duration != max_end_time:
+                # Get the end time
+                end_time = start_time + duration
+                # Encode the clipped part
+                enc = encode_midi(mid, start_time, end_time)
+            else:
+                encodings.append(enc)
+                start_time = end_time + 1
+
+            start_time += (duration - duration / 10)
+
+    else:
+        if duration != max_end_time:
+            # Get a start time, max_end_time should be equal to duration in worst case
+            start_time = random.uniform(0, max_end_time - duration)
+            # Ensure the start time is at least 0
+            start_time = 0 if start_time < 0 else start_time
+            # Get the end time
+            end_time = start_time + duration
+            # Encode the clipped part
+            enc = encode_midi(mid, start_time, end_time)
+            encodings.append(enc)
+
+    return encodings
+
+
+def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, max_seq=1024, keys=False, full_seq=False):
     """
 
     Author: Corentin Nelias
@@ -116,10 +142,18 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2):
         else:
             split_type = "test"
             
-        mid = piece
         f_name = piece.split('/')[-1].split('.')[0] + ".pickle"
         try:
-            prepped = midi_processor.encode_midi(mid)
+            mid = pretty_midi.PrettyMIDI(midi_file=piece)
+            enc = encode_midi(piece)
+            encodings = time_split(mid, enc, max_seq, keys, full_seq)
+            # Key operations
+            if keys:
+                my_score: music21.stream.Score = music21.converter.parse(piece)
+                key = my_score.analyze('Krumhansl')
+            else:
+                key = None
+
         except Exception as e:
             print(e)
             continue
@@ -133,7 +167,7 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2):
             o_file = os.path.join(test_dir, f_name)
             test_count += 1
         
-        dump((mid, prepped), o_file)
+        dump((str(key), encodings, mid, piece), o_file)
 
         total_count += 1
 
@@ -154,7 +188,10 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("root", type=str, help="Root folder for the data.")
-    parser.add_argument("-output_dir", type=str, default="./dataset/e_piano", help="Output folder to put the preprocessed midi into.")
+    parser.add_argument("--output_dir", type=str, default="./dataset/e_piano", help="Output folder to put the preprocessed midi into.")
+    parser.add_argument('--max_seq', type=int, default=1024, help='Max sequence length for each data')
+    parser.add_argument("--keys", action='store_true', help="Enable key data?")
+    parser.add_argument("--full_seq", action='store_true', help="Encode full sequence data")
 
     return parser.parse_args()
 
@@ -170,7 +207,7 @@ def main():
     output_dir = args.output_dir
 
     print(f"Preprocessing midi files and saving to {output_dir}")
-    prep_custom_midi(root, output_dir)
+    prep_custom_midi(root, output_dir, max_seq=args.max_seq, keys=args.keys, full_seq=args.full_seq)
     print("Done!")
     
 
