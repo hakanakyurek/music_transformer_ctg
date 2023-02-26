c881a393740167dac5090dcebaf6637e57b46f73
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-02-26 18:06:27 +0100

    preprocessing now supports classification dataset creation

--- preprocess_midi.py
+++ preprocess_midi.py
@@ -10,6 +10,8 @@ from lib.midi_processor.processor import encode_midi, decode_midi
 import music21
 import pretty_midi
 
+import pandas as pd
+
 
 def key_custom_midi(pieces, output_dir):
     """
@@ -46,14 +48,14 @@ def key_custom_midi(pieces, output_dir):
     return True
 
 
-def time_split(mid, enc, max_seq, keys=False, full_seq=False):
+def time_split(mid, enc, max_seq, task='keys_ctrl', full_seq=False):
     
     encodings = []
     # Get the end time of the whole midi
     max_end_time = mid.get_end_time()
     # Decode back the encoding
     # Read time for max_seq - 1 tokens if we are using keys
-    max_seq = max_seq if not keys else max_seq - 1
+    max_seq = max_seq if not task == 'keys_ctrl' else max_seq - 1
 
     time_check_midi = decode_midi(enc[0:max_seq])
 
@@ -96,7 +98,7 @@ def time_split(mid, enc, max_seq, keys=False, full_seq=False):
     return encodings
 
 
-def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, max_seq=1024, keys=False, full_seq=False):
+def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, max_seq=1024, task='keys_ctrl', full_seq=False):
     """
 
     Author: Corentin Nelias
@@ -107,9 +109,9 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, 
     """
 
     if full_seq:
-        output_dir = os.path.join(output_dir, "full_seq")
+        output_dir = os.path.join(output_dir, "full_seq", task)
     else:
-        output_dir = os.path.join(output_dir, "random_seq") 
+        output_dir = os.path.join(output_dir, "random_seq", task) 
 
     train_dir = os.path.join(output_dir, "train")
     os.makedirs(train_dir, exist_ok=True)
@@ -134,7 +136,12 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, 
     print(f"Found {len(pieces)} pieces")
     print("Preprocessing data...")
 
+    artist_df = pd.read_csv(os.path.join(custom_midi_root, 'maestro-v3.0.0/maestro-v3.0.0.csv'))
+
+    a = set()
+
     for piece in tqdm(pieces):
+        print(piece)
         #deciding whether the data should be part of train, valid or test dataset
         is_train = True if random.random() > valid_p else False
         if not is_train:
@@ -148,6 +155,32 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, 
             
         f_name = piece.split('/')[-1].split('.')[0] + ".pickle"
         try:
+            # Key operations
+            if task == 'keys' or task == 'keys_ctrl':
+                my_score: music21.stream.Score = music21.converter.parse(piece)
+                y = my_score.analyze('Krumhansl')
+            elif task == 'artist':
+                # Only for maestro
+                artist = artist_df[artist_df['midi_filename'].str.contains(piece.split('/')[-1])]['canonical_composer']
+                if len(artist) != 0:
+                    y = artist.iloc[0]
+                    a.add(y.split(' / ')[0])
+                else:
+                    # Data is not in maestro
+                    continue
+            elif task == 'genre':
+                dataset = piece.split('/')[2]
+                if dataset == 'maestro-v3.0.0':
+                    y = 'classical'
+                elif dataset == 'vgmidi':
+                    y = 'soundtracks'
+                elif dataset == 'piano-midi':
+                    y = 'classical'
+                elif dataset == 'adl-piano-midi':
+                    y = piece.split('/')[3].lower()
+            else:
+                y = None
+
             mid = pretty_midi.PrettyMIDI(midi_file=piece)
             earliest_note_start = min(note.start for inst in mid.instruments for note in inst.notes)
             # Subtract the earliest note start time from the start time of each note
@@ -155,15 +188,10 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, 
                 [setattr(note, 'start', note.start - earliest_note_start) for note in inst.notes]
                 [setattr(note, 'end', note.end - earliest_note_start) for note in inst.notes]
             enc = encode_midi(mid)
-            encodings = time_split(mid, enc, max_seq, keys, full_seq)
+            encodings = time_split(mid, enc, max_seq, task, full_seq)
             if encodings is None:
                 continue
-            # Key operations
-            if keys:
-                my_score: music21.stream.Score = music21.converter.parse(piece)
-                key = my_score.analyze('Krumhansl')
-            else:
-                key = None
+        
 
         except Exception as e:
             print(e)
@@ -178,9 +206,12 @@ def prep_custom_midi(custom_midi_root, output_dir, valid_p = 0.1, test_p = 0.2, 
             o_file = os.path.join(test_dir, f_name)
             test_count += 1
         
-        dump((str(key), encodings, piece), o_file)
+        dump((str(y), encodings, piece), o_file)
 
         total_count += 1
+
+    my_dict = {elem: idx for idx, elem in enumerate(a)}
+    print(my_dict)
 
     print(f"Num Train: {train_count}")
     print(f"Num Val: {val_count}")
@@ -201,8 +232,9 @@ def parse_args():
     parser.add_argument("root", type=str, help="Root folder for the data.")
     parser.add_argument("--output_dir", type=str, default="./dataset/e_piano", help="Output folder to put the preprocessed midi into.")
     parser.add_argument('--max_seq', type=int, default=1024, help='Max sequence length for each data')
-    parser.add_argument("--keys", action='store_true', help="Enable key data?")
+    parser.add_argument("--task", type=str, default='normal_lm', help="What should be the class data: key, artist(for maestro), genre")
     parser.add_argument("--full_seq", action='store_true', help="Encode full sequence data")
+
 
     return parser.parse_args()
 
@@ -220,7 +252,7 @@ def main():
     random.seed(10)
 
     print(f"Preprocessing midi files and saving to {output_dir}")
-    prep_custom_midi(root, output_dir, max_seq=args.max_seq, keys=args.keys, full_seq=args.full_seq)
+    prep_custom_midi(root, output_dir, max_seq=args.max_seq, task=args.task, full_seq=args.full_seq)
     print("Done!")
     
 
