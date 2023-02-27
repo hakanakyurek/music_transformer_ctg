e727245f7b2848e67d0caf7b36dd9ab2e09d74a8
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-02-27 11:34:28 +0100

    task of datset should be correctly set

--- lib/data/classification_data_module.py
+++ lib/data/classification_data_module.py
@@ -5,7 +5,7 @@ import torch
 
 class ClassificationDataModule(pl.LightningDataModule):
     
-    def __init__(self, batch_size, data_dir, dataset_percentage, max_seq, n_workers) -> None:
+    def __init__(self, batch_size, data_dir, dataset_percentage, max_seq, n_workers, task) -> None:
         super().__init__()
 
         self.batch_size = batch_size
@@ -13,6 +13,7 @@ class ClassificationDataModule(pl.LightningDataModule):
         self.n_workers = n_workers
         self.data_dir = data_dir
         self.dataset_percentage = dataset_percentage
+        self.task = task
 
     def collate(self, batch):
 
@@ -24,7 +25,7 @@ class ClassificationDataModule(pl.LightningDataModule):
         return x, y
 
     def train_dataloader(self):
-        self.train = ClassificationDataset(f'{self.data_dir}train/', self.max_seq, self.dataset_percentage)
+        self.train = ClassificationDataset(f'{self.data_dir}train/', self.max_seq, self.dataset_percentage, self.task)
         print('Train dataset size:', len(self.train))
         return  DataLoader(self.train, batch_size=self.batch_size, 
                            collate_fn=self.collate,
@@ -32,7 +33,7 @@ class ClassificationDataModule(pl.LightningDataModule):
                            drop_last=True)
 
     def val_dataloader(self):
-        self.val = ClassificationDataset(f'{self.data_dir}val/', self.max_seq, self.dataset_percentage)
+        self.val = ClassificationDataset(f'{self.data_dir}val/', self.max_seq, self.dataset_percentage, self.task)
         print('Val dataset size:', len(self.val))
         return  DataLoader(self.val, batch_size=self.batch_size, 
                            collate_fn=self.collate,
@@ -40,7 +41,7 @@ class ClassificationDataModule(pl.LightningDataModule):
                            drop_last=True)
 
     def test_dataloader(self):
-        self.test = ClassificationDataset(f'{self.data_dir}test/', self.max_seq, self.dataset_percentage)
+        self.test = ClassificationDataset(f'{self.data_dir}test/', self.max_seq, self.dataset_percentage, self.task)
         print('Test dataset size:', len(self.test))
         return  DataLoader(self.test, batch_size=self.batch_size, 
                            collate_fn=self.collate,
