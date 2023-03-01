cc89d97d9f58529a237dbfc5f1d61db0f96b5d93
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-03-01 13:36:30 +0100

    testing script should calculate accuracy and print target and primer classes

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 import os
-import random
+import music21
 from tqdm import tqdm
 
 from lib.midi_processor.processor import decode_midi, encode_midi
@@ -11,6 +11,8 @@ from lib.utilities.device import get_device, use_cuda
 from lib.utilities.constants import *
 from lib.data.midi_processing import *
 
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 # main
 def test(piece, output_dir, args):
@@ -19,7 +21,7 @@ def test(piece, output_dir, args):
     Entry point. Generates music from a model specified by command line arguments
 
     """
-    
+    classes = [None, None]    
 
     raw_mid = encode_midi(piece)
     if(len(raw_mid) == 0):
@@ -32,6 +34,10 @@ def test(piece, output_dir, args):
     # Saving primer first
     f_path = os.path.join(output_dir, "primer.mid")
     decode_midi(primer[:args.num_prime].cpu().numpy(), file_path=f_path)
+    if args.keys:
+        my_score: music21.stream.Score = music21.converter.parse(f_path)
+        key_primer = my_score.analyze('Krumhansl')
+        classes[0] = key_primer
 
     # GENERATION
     model.eval()
@@ -41,7 +47,14 @@ def test(piece, output_dir, args):
                                   temperature=args.temperature, top_k=args.top_k, top_p=args.top_p)
 
         f_path = os.path.join(output_dir, "rand.mid")
-        decode_midi(rand_seq[0].cpu().numpy(), file_path=f_path)
+        rand_seq = rand_seq[0].cpu().numpy()
+        decode_midi(rand_seq, file_path=f_path)
+        if args.keys:
+            my_score: music21.stream.Score = music21.converter.parse(f_path)
+            key_rand = my_score.analyze('Krumhansl')
+            classes[1] = key_rand
+
+    return raw_mid[:len(rand_seq)], rand_seq, classes
 
 
 if __name__ == "__main__":
@@ -72,13 +85,32 @@ if __name__ == "__main__":
 
     print(f"Found {len(pieces)} pieces")
 
+    # Metrics
+    overall_acc = 0
+    per_piece_accuracy = []
+    primer_classes = []
+    output_classes = []
+
     # Can be None, an integer index to dataset
     if(args.primer_index is None):
         for piece in tqdm(pieces):
             output_dir = os.path.join(args.output_dir, piece)
-            test(piece, output_dir)
-
+            raw_mid, rand_seq, classes = test(args, piece, output_dir)
+            p_acc = accuracy_score(raw_mid, rand_seq) 
+            per_piece_accuracy.append(p_acc)
+            primer_classes.append(classes[0])
+            output_classes.append(classes[1])
     else:
         piece = pieces[args.primer_index]
+        output_dir = os.path.join(args.output_dir, piece)
         print(f"Using primer file: {piece}")
-        test(args, piece)
+        raw_mid, rand_seq, classes = test(args, piece, output_dir)
+        p_acc = accuracy_score(raw_mid, rand_seq) 
+        per_piece_accuracy.append(p_acc)
+        primer_classes.append(classes[0])
+        output_classes.append(classes[1])
+
+    overall_acc = np.mean(per_piece_accuracy)
+
+    print(per_piece_accuracy)
+    print(overall_acc)
