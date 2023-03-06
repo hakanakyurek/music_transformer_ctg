e55461ddd9aadc60065c682c2569a217a12fd3d1
Hakan Akyürek <hakan.akyuerek@tum.de>
2023-03-06 16:24:57 +0100

    classification model creation should work for both training and test

--- lib/utilities/create_model.py
+++ lib/utilities/create_model.py
@@ -73,10 +73,14 @@ def create_model_for_generation(args):
 
 
 def create_model_for_classification(args, loss_func, n_classes):
-    temp = args.key
-    args.key = False
-    music_transformer = create_model_for_generation(args)
-    args.key = temp
+    try:
+        temp = args.key
+        args.key = False
+        music_transformer = create_model_for_generation(args)
+        args.key = temp
+    except:
+        args.key = False
+        music_transformer = create_model_for_generation(args)
 
     model = MusicTransformerClassifier(music_transformer=music_transformer,
                                        n_classes=n_classes,
